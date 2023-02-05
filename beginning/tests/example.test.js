5ec1da0c624064b7b1831ea664d63d2e4f5fbd22
andre godeiro <andrevictor.godeiro@gmail.com>
2023-02-05 16:12:00 -0300

    set default timeouts

--- beginning/tests/example.test.js
+++ beginning/tests/example.test.js
@@ -3,18 +3,22 @@ const expect = require('chai').expect
 
 describe('My first puppeteer test', () => {
     it('should launch the browser', async () => {
-        const browser = await puppeteer.launch({headless: true, slowMo: 10, devtools: false})
+        const browser = await puppeteer.launch({headless: false, slowMo: 10, devtools: false})
         const page = await browser.newPage()
+
+        await page.setDefaultTimeout(10000) //set a default timeout
+        await page.setDefaultNavigationTimeout(20000) //this method has priority over setDefaultTimeout()
+
         await page.goto('http://example.com/')
         const title = await page.title()
         const url = await page.url()
         const text = await page.$eval('h1', element => element.textContent)
         const count = await page.$$eval('p', element => element.length)
 
-        expect(title).to.be.a('string', 'Example Domain') //compare type and string content
-        expect(url).to.include('example.com') //compare if the url includes something
+        expect(title).to.be.a('string', 'Example Domain') 
+        expect(url).to.include('example.com') 
         expect(text).to.to.be.a('string', 'Example Doimain')
-        expect(count).to.equal(2) //compare if the count number its 2
+        expect(count).to.equal(2) 
 
         await browser.close()
     })
