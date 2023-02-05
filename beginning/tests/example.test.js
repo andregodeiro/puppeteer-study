65c837a093f4ef4c00b17afe85616067aa4ebe6b
andre godeiro <andrevictor.godeiro@gmail.com>
2023-02-05 14:20:43 -0300

    go back and go foward in broser

--- beginning/tests/example.test.js
+++ beginning/tests/example.test.js
@@ -2,14 +2,16 @@ const puppeteer = require('puppeteer');
 
 describe('My frist puppeteer test', () => {
     it('should launch the browser', async () => {
-        const browser = await puppeteer.launch({headless: false, slowMo: 10, devtools: false})
+        const browser = await puppeteer.launch({headless: false, slowMo: 250, devtools: false})
         const page = await browser.newPage()
         await page.goto('http://example.com/')
-        await page.waitForTimeout(3000)
         await page.waitForSelector('h1')
-        await page.reload()
-        await page.waitForTimeout(3000)
+        await page.goto('http://dev.to/')
+        await page.waitForSelector('#header-search')
+        await page.goBack()
         await page.waitForSelector('h1')
+        await page.goForward()
+        await page.waitForSelector('#header-search')
         await browser.close()
     })
 })
