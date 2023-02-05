e43c7fb23e0d5641a0d6538b2ab74efe5f3f64f0
andre godeiro <andrevictor.godeiro@gmail.com>
2023-02-05 16:03:59 -0300

    assertions with puppeteer and chai

--- beginning/tests/example.test.js
+++ beginning/tests/example.test.js
@@ -1,17 +1,21 @@
 const puppeteer = require('puppeteer');
+const expect = require('chai').expect
 
-describe('My frist puppeteer test', () => {
+describe('My first puppeteer test', () => {
     it('should launch the browser', async () => {
-        const browser = await puppeteer.launch({headless: false, slowMo: 10, devtools: false})
+        const browser = await puppeteer.launch({headless: true, slowMo: 10, devtools: false})
         const page = await browser.newPage()
         await page.goto('http://example.com/')
         const title = await page.title()
         const url = await page.url()
         const text = await page.$eval('h1', element => element.textContent)
-        const count = await page.$$eval('p', element => element.length) // the double $$ means that we are selecting multiple elements
+        const count = await page.$$eval('p', element => element.length)
 
-        console.log('Number of p tags on the page: ' + count)
-        console.log('Text in h1: ' + text)
+        expect(title).to.be.a('string', 'Example Domain') //compare type and string content
+        expect(url).to.include('example.com') //compare if the url includes something
+        expect(text).to.to.be.a('string', 'Example Doimain')
+        expect(count).to.equal(2) //compare if the count number its 2
+
         await browser.close()
     })
 })
