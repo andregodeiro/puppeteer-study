8a1a0298c15b201819e4d5a9587a5a60ac767518
andre godeiro <andrevictor.godeiro@gmail.com>
2023-02-05 14:00:21 -0300

    pause test with waitForTimeout

--- beginning/tests/example.test.js
+++ beginning/tests/example.test.js
@@ -5,6 +5,7 @@ describe('My frist puppeteer test', () => {
         const browser = await puppeteer.launch({headless: false, slowMo: 10, devtools: false})
         const page = await browser.newPage()
         await page.goto('http://example.com/')
+        await page.waitForTimeout(10000)
         await page.waitForSelector('h1')
         await browser.close()
     })
