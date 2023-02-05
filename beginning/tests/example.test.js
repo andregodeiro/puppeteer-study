4c472747ce120a568a12606f4601e8eb87a9359e
andre godeiro <andrevictor.godeiro@gmail.com>
2023-02-05 17:01:14 -0300

    using xpath to select element and type something

--- beginning/tests/example.test.js
+++ beginning/tests/example.test.js
@@ -6,14 +6,16 @@ describe('My first puppeteer test', () => {
         const browser = await puppeteer.launch({headless: false, slowMo: 10, devtools: false})
         const page = await browser.newPage()
 
-        await page.setDefaultTimeout(10000) //set a default timeout
-        await page.setDefaultNavigationTimeout(20000) //this method has priority over setDefaultTimeout()
+        await page.setDefaultTimeout(10000)
+        await page.setDefaultNavigationTimeout(20000)
 
         await page.goto('http://google.com/')
 
-        await page.waitForXPath('//input[@Class="gLFyf"]') //here is and exemple of how to use XPath on puppeteer
+        await page.waitForXPath('//input[@class="gLFyf"]') //here is an exemple of how to use XPath on puppeteer
         
-        await page.type('.gLFyf', 'github') 
+        const input = await page.$x('//input[@class="gLFyf"]') //the method $x() its used to catch a XPath address
+        await input[0].type('github')  //here we catch the first array element, even if input returns only one element
+
         await page.keyboard.press('Enter', {delay: 1000}) 
         await page.waitForTimeout(5000)
 
