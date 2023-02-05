1b7b3c8d029bdec4f97c803c8fbd32c4ff7000d8
andre godeiro <andrevictor.godeiro@gmail.com>
2023-02-05 14:28:37 -0300

    typing on inputs and adding delay on action

--- beginning/tests/example.test.js
+++ beginning/tests/example.test.js
@@ -2,16 +2,11 @@ const puppeteer = require('puppeteer');
 
 describe('My frist puppeteer test', () => {
     it('should launch the browser', async () => {
-        const browser = await puppeteer.launch({headless: false, slowMo: 250, devtools: false})
+        const browser = await puppeteer.launch({headless: false, slowMo: 10, devtools: false})
         const page = await browser.newPage()
-        await page.goto('http://example.com/')
-        await page.waitForSelector('h1')
-        await page.goto('http://dev.to/')
-        await page.waitForSelector('#header-search')
-        await page.goBack()
-        await page.waitForSelector('h1')
-        await page.goForward()
-        await page.waitForSelector('#header-search')
+        await page.goto('https://devexpress.github.io/testcafe/example/')
+        await page.type('#developer-name', 'Godeiro', {delay: 0})
+        await page.waitForTimeout(5000)
         await browser.close()
     })
 })
