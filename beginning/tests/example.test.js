1d415925b7128ad901f9567ecf3d26552c7f4f94
andre godeiro <andrevictor.godeiro@gmail.com>
2023-02-05 13:58:04 -0300

    slowMode and devtools

--- beginning/tests/example.test.js
+++ beginning/tests/example.test.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 describe('My frist puppeteer test', () => {
     it('should launch the browser', async () => {
-        const browser = await puppeteer.launch({headless: false})
+        const browser = await puppeteer.launch({headless: false, slowMo: 10, devtools: false})
         const page = await browser.newPage()
         await page.goto('http://example.com/')
         await page.waitForSelector('h1')
