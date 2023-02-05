24e6a8d9f150c72f8c3c6cc932b0a2ddfa21061f
andre godeiro <andrevictor.godeiro@gmail.com>
2023-02-05 14:53:31 -0300

    get page title and url

--- beginning/tests/example.test.js
+++ beginning/tests/example.test.js
@@ -4,15 +4,12 @@ describe('My frist puppeteer test', () => {
     it('should launch the browser', async () => {
         const browser = await puppeteer.launch({headless: false, slowMo: 10, devtools: false})
         const page = await browser.newPage()
-        await page.goto('https://devexpress.github.io/testcafe/example/')
-        await page.type('#developer-name', 'Godeiro', {delay: 0})
-        await page.click('#tried-test-cafe')
-        await page.select('#preferred-interface', 'JavaScript API')
-        const message = 'lets fill the message with some text' //you can store the message in a const or pass it as the second argument after the selector
-        await page.type('#comments', message)
-        await page.click('#submit-button')
-        await page.waitForSelector('.result-content')
-        await page.waitForTimeout(5000)
+        await page.goto('http://example.com/')
+        const title = await page.title()
+        const url = await page.url()
+
+        console.log('Title: ' + title)
+        console.log('URL: ' + url)
         await browser.close()
     })
 })
