a804af8d7ee1605e099dcc26fd085bd18dbf0cd3
André Godeiro <andrevictor.godeiro@gmail.com>
2023-02-06 15:30:12 -0300

    custom methods pt.2

--- 02_advanced/lib/helpers.js
+++ 02_advanced/lib/helpers.js
@@ -26,4 +26,35 @@ module.exports = {
       throw new Error(`Cannot get count of selector: ${selector}`);
     }
   },
+  typeText: async (page, selector, text) => {
+    //create a method that will auto check the selector and type some text into it
+    try {
+      await page.waitForSelector(selector);
+      await page.type(selector, text);
+    } catch (error) {
+      throw new Error(`Could not type into selector: ${selector}`);
+    }
+  },
+  waitForText: async (page, selector, text) => {
+    //create a method that will check if the selector matches with text
+    try {
+      await page.waitForSelector(selector);
+      await page.waitForFunction((selector, text) => {
+        document.querySelector(selector).innerText.includes(text),
+          {},
+          selector,
+          text;
+      });
+    } catch (error) {
+      throw new Error(`Text: ${text} not found for selector: ${selector}`);
+    }
+  },
+  shouldNotExist: async (page, selector, text) => {
+    //create a method that will auto check the selector and type some text into it
+    try {
+      await page.waitForSelector(selector, { hidden: true, timeout: 300 });
+    } catch (error) {
+      throw new Error(`Selector: ${selector} is visible, bou should not be.`);
+    }
+  },
 };
--- 02_advanced/tests/advanced.test.js
+++ 02_advanced/tests/advanced.test.js
@@ -1,6 +1,13 @@
 const puppeteer = require("puppeteer");
 const expect = require("chai").expect;
-const { click, getText, getCount } = require("../lib/helpers"); //import custom commands
+const {
+  click,
+  getText,
+  getCount,
+  typeText,
+  waitForText,
+  shouldNotExist,
+} = require("../lib/helpers"); //import custom commands
 
 describe("My first puppeteer test", () => {
   let browser;
@@ -23,18 +30,21 @@ describe("My first puppeteer test", () => {
   it("should launch the browser", async () => {
     await page.goto("http://google.com/");
 
-    const text = await getText(page, ".gNO89b"); //store methods on const
+    const text = await getText(page, ".gNO89b");
     const count = await getCount(page, ".gNO89b");
 
-    await page.waitForXPath('//input[@class="gLFyf"]');
-    const input = await page.$x('//input[@class="gLFyf"]');
-    await input[0].type("github");
+    await typeText(page, ".gNO89b", "github"); //will do the same as this block of code above.
+
+    // await page.waitForXPath('//input[@class="gLFyf"]');
+    // const input = await page.$x('//input[@class="gLFyf"]');
+    // await input[0].type("github");
 
     await page.waitForXPath('//input[@value="Pesquisa Google"]');
 
-    await click(page, ".gNO89b"); //apllying the method. page and selector as paramethers
+    await click(page, ".gNO89b");
 
-    await page.waitForSelector(".gNO89b", { hidden: true, timeout: 300 });
+    await shouldNotExist(page, ".gNO89b"); //will check if the selector disapeared of the page
+
     await page.waitForTimeout(5000);
   });
 });
