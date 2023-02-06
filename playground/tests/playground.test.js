f867f7f5c3993c78548f0f96d6b9b7397971d353
André Godeiro <andrevictor.godeiro@gmail.com>
2023-02-06 11:01:48 -0300

    input text test

--- playground/tests/playground.test.js
+++ playground/tests/playground.test.js
@@ -1,23 +1,31 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require("puppeteer");
+require("dotenv").config();
 
-describe('open github and sign in', () => {
-    it('should launch github', async () => {
-        const browser = await puppeteer.launch({headless: false, defaultViewport: null})
-        const page = await browser.newPage()
+const email = process.env.EMAIL;
+const password = process.env.PASSWORD;
 
-        await page.goto('http://github.com')
-        
-        await page.waitForXPath('//a[contains(text(), "Sign in")]')
-        const signIn = await page.$x('//a[contains(text(), "Sign in")]')
-        await signIn[0].click()
+describe("open github and sign in", () => {
+  it("should launch github", async () => {
+    const browser = await puppeteer.launch({
+      headless: false,
+      defaultViewport: { width: 1920, height: 1080 },
+      args: ["--start-maximized"],
+      slowMo: 100,
+    });
+    const page = await browser.newPage();
 
-        await page.waitForXPath('//h1[@value="Sign in to GitHub"]')
+    await page.goto("http://github.com");
 
-        await page.waitForTimeout(3000)
+    await page.waitForXPath('//a[contains(text(), "Sign in")]');
+    const signIn = await page.$x('//a[contains(text(), "Sign in")]');
+    await signIn[0].click();
 
-        await browser.close()
+    await page.waitForXPath('//input[@id="login_field"]');
+    const login_field = await page.$x('//input[@id="login_field"]');
+    await login_field[0].type(`${email}`);
 
-    
+    await page.waitForTimeout(3000);
 
-    })
-})
+    await browser.close();
+  });
+});
