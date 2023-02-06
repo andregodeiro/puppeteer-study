b0f47a32926a528f6dc1f775264a32667082ce3f
André Godeiro <andrevictor.godeiro@gmail.com>
2023-02-06 11:39:37 -0300

    advanced module

--- playground/tests/playground.test.js
+++ playground/tests/playground.test.js
@@ -1,8 +1,8 @@
 const puppeteer = require("puppeteer");
 require("dotenv").config();
 
-const email = process.env.EMAIL;
-const password = process.env.PASSWORD;
+const git_email = process.env.EMAIL;
+const git_password = process.env.PASSWORD;
 
 describe("open github and sign in", () => {
   it("should launch github", async () => {
@@ -22,7 +22,15 @@ describe("open github and sign in", () => {
 
     await page.waitForXPath('//input[@id="login_field"]');
     const login_field = await page.$x('//input[@id="login_field"]');
-    await login_field[0].type(`${email}`);
+    await login_field[0].type(`${git_email}`);
+
+    await page.waitForXPath('//input[@id="password"]');
+    const password = await page.$x('//input[@id="password"]');
+    await password[0].type(`${git_password}`);
+
+    await page.waitForXPath('//input[@value="Sign in"]');
+    const signInButton = await page.$x('//input[@value="Sign in"]');
+    await signInButton[0].click();
 
     await page.waitForTimeout(3000);
 
