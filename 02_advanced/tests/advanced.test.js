464cdddad13902b9bd769b78bd7ab4c93a25fcd5
André Godeiro <andrevictor.godeiro@gmail.com>
2023-02-06 13:13:49 -0300

    hooks with mocha

---
+++ 02_advanced/tests/advanced.test.js
@@ -0,0 +1,58 @@
+const puppeteer = require("puppeteer");
+const expect = require("chai").expect;
+
+describe("My first puppeteer test", () => {
+  let browser;
+  let page;
+
+  before(async () => {
+    //this hook runs before all the tests starts
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 10,
+      devtools: false,
+    });
+
+    page = await browser.newPage();
+  });
+
+  after(async () => {
+    //this hook runs after all the tests end
+    await browser.close();
+  });
+
+  beforeEach(async () => {
+    //this hook runs before each it block
+  });
+
+  afterEach(async () => {
+    //this hook runs after each it block
+  });
+
+  it("should launch the browser", async () => {
+    // const browser = await puppeteer.launch({
+    //   headless: false,
+    //   slowMo: 10,
+    //   devtools: false,
+    // });
+    // const page = await browser.newPage();
+
+    await page.goto("http://google.com/");
+
+    await page.waitForXPath('//input[@class="gLFyf"]');
+
+    const input = await page.$x('//input[@class="gLFyf"]');
+    await input[0].type("github");
+
+    await page.waitForXPath('//input[@value="Pesquisa Google"]');
+
+    const button = await page.$x('//input[@value="Pesquisa Google"]');
+    await button[0].click();
+
+    await page.waitForFunction(() => !document.querySelector(".gNO89b")); //longer option
+
+    await page.waitForSelector(".gNO89b", { hidden: true, timeout: 300 }); //shorter version
+
+    await page.waitForTimeout(5000);
+  });
+});
